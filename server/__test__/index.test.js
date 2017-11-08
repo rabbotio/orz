77a5f3a7cb65285c4747ab932c1cc7f49e461631
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-11-09 00:21:25 +0700

    test(token): Working get token

--- server/__test__/index.test.js
+++ server/__test__/index.test.js
@@ -1,26 +1,29 @@
 require('../pre')
 
 describe('authenticate()', () => {
-  it('should authenticate the request', async () => {
+  it('should authorize and return tokens', async () => {
+    // Helper
+    const qs = require('qs')
+
+    // Server
     const nap = require('../nap')
     await nap.start(require('./config'))
 
+    // Config
     const { base_url } = require('./config')
-    const authorize_url = `${base_url}/oauth/authorize`
+    const oauth_authorize_url = `${base_url}/oauth/authorize/`
 
-    const query = require('qs').stringify({
+    const queryCode = qs.stringify({
       redirect_uri: base_url,
       response_type: 'code',
       client_id: 1,
       scope: 'profile'
     })
 
-    console.log(`${authorize_url}/?${query}`)
+    // #1 : redirect_uri -> code
 
-    const result = await fetch(`${authorize_url}/?${query}`)
-
+    const result = await fetch(`${oauth_authorize_url}?${queryCode}`)
     const json = await result.json()
-    console.log(json)
 
     expect(json).toMatchObject({
       redirectUri: expect.stringMatching(new RegExp(String.raw`http:\/\/localhost:3030\/\?scope=profile&expires_in=3600&code=`)),
@@ -30,6 +33,26 @@ describe('authenticate()', () => {
         client_id: '1',
         scope: ['profile']
       }
+    })
+
+    // #2 : code -> token
+    const oauth_token_url = `${base_url}/oauth/token/`
+
+    const queryToken = qs.stringify({
+      grant_type: 'authorization_code',
+      client_id: '1',
+      client_secret: 'kittens',
+      code: json.redirectUri.split('&code=')[1]
+    })
+    console.log(`${oauth_token_url}?${queryToken}`)
+    const token_result = await fetch(`${oauth_token_url}?${queryToken}`)
+    const token_json = await token_result.json()
+
+    expect(token_json).toMatchObject({
+      token_type: 'Bearer',
+      expires_in: expect.any(Number),
+      access_token: expect.any(String),
+      refresh_token: expect.any(String)
     })
 
     await nap.stop()
